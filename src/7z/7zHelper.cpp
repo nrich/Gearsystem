c9c28918b11b32cd925883d119b705d0ac0ea55f
Neil Richardson <nrich@neiltopia.com>
2025-03-16 12:52:15 +0800

    Hack utf16le -> utf8

--- src/7z/7zHelper.cpp
+++ src/7z/7zHelper.cpp
@@ -130,7 +130,13 @@ n7z::Archive::Archive(const std::string &filename)
 
                 uint64_t file_size = SzArEx_GetFileSize(&db, i);
 
-                std::string path = utf16_to_utf8(std::u16string(reinterpret_cast<char16_t*>(temp), len));
+                //std::string path = utf16_to_utf8(std::u16string(reinterpret_cast<char16_t*>(temp), len));
+                std::string path;
+                // TODO this will need looking at
+                for (size_t i = 0; i < len; i++)
+                {
+                    path.push_back((char)(temp[i] & 0xFF));
+                }
 
                 entries.push_back(n7z::FileEntry{path, file_size, i});
             }
